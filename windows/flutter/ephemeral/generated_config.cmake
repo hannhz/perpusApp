e4911cffe56c8288625859cdeb1ce6f534924cf8
hannhz <hanan.hafizhah@gmail.com>
2025-09-02 17:28:15 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\APP\\android studio\\flutter_windows_3.29.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\APP\\vscode\\Manajemen_Buku\\book_management" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\APP\\android studio\\flutter_windows_3.29.0-stable\\flutter"
+  "PROJECT_DIR=D:\\APP\\vscode\\Manajemen_Buku\\book_management"
+  "FLUTTER_ROOT=D:\\APP\\android studio\\flutter_windows_3.29.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\APP\\vscode\\Manajemen_Buku\\book_management\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\APP\\vscode\\Manajemen_Buku\\book_management"
+  "FLUTTER_TARGET=D:\\APP\\vscode\\Manajemen_Buku\\book_management\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\APP\\vscode\\Manajemen_Buku\\book_management\\.dart_tool\\package_config.json"
+)
